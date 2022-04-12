e45472fa8908666d0040346351c5ab7b34ac217f
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-04-12 11:20:47 +0300

    changes

--- src/main/java/org/sparkexample/WordCountTask.java
+++ src/main/java/org/sparkexample/WordCountTask.java
@@ -10,20 +10,10 @@ import java.util.Arrays;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-/**
- * WordCountTask class, we will call this class with the test WordCountTest.
- */
-public class WordCountTask {
-  /**
-   * We use a logger to print the output. Sl4j is a common library which works with log4j, the
-   * logging system used by Apache Spark.
-   */
+public class WordCount {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(WordCountTask.class);
 
-  /**
-   * This is the entry point when the task is called from command line with spark-submit.sh.
-   * See {@see http://spark.apache.org/docs/latest/submitting-applications.html}
-   */
   public static void main(String[] args) {
     checkArgument(args.length > 0, "Please provide the path of input file as first parameter.");
     new WordCountTask().run(args[0]);
@@ -33,24 +23,14 @@ public class WordCountTask {
    * The task body
    */
   public void run(String inputFilePath) {
-    /*
-     * This is the address of the Spark cluster. We will call the task from WordCountTest and we
-     * use a local standalone cluster. [*] means use all the cores available.
-     * See {@see http://spark.apache.org/docs/latest/submitting-applications.html#master-urls}.
-     */
+
     String master = "local[*]";
 
-    /*
-     * Initialises a Spark context.
-     */
     SparkConf conf = new SparkConf()
         .setAppName(WordCountTask.class.getName())
         .setMaster(master);
     JavaSparkContext context = new JavaSparkContext(conf);
-
-    /*
-     * Performs a work count sequence of tasks and prints the output with a logger.
-     */
+    
     context.textFile(inputFilePath)
         .flatMap(text -> Arrays.asList(text.split(" ")).iterator())
         .mapToPair(word -> new Tuple2<>(word, 1))
